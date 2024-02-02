02de64a6106e0d8efc2df1956729995c25c81dce
Dark-Matter1 <ben.w.1516@gmail.com>
2024-02-02 11:58:36 -0500

    Added Lab03-classactivity

---
+++ PowershellLabs/Lab03-ClassActivity/ps_lab03-1-Main.ps1
@@ -0,0 +1,9 @@
+﻿cd $PSScriptRoot
+
+. .\ps_lab03-1-all.ps1
+
+Get-LogonEvent 15
+
+Get-ShutdownEvent 25
+
+Get-StartupEvent 25
---
+++ PowershellLabs/Lab03-ClassActivity/ps_lab03-1-all.ps1
@@ -0,0 +1,77 @@
+﻿# Powershell Script
+# SYS-320 - Ben W
+
+function Get-LogonEvent($daysOld) {
+
+    $winlogon = Get-EventLog system -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$daysOld)
+
+    $winlogonTable = @()
+
+    for ($i=0; $i -lt $winlogon.count; $i++) {
+    
+        $event = "none"
+        if ($winlogon[$i].InstanceId -eq "7001") { $event="Logon" } 
+        if ($winlogon[$i].InstanceId -eq "7002")  { $event="Logoff" }
+
+        $userid = $winlogon[$i].ReplacementStrings[1]
+
+        $userid = New-Object System.Security.Principal.SecurityIdentifier($userid)
+
+        $user = $userid.Translate([System.Security.Principal.NTAccount])
+
+        $winlogonTable += [pscustomobject]@{"Time" = $winlogon[$i].TimeGenerated;
+                                              "Id" = $winlogon[$i].InstanceId;
+                                           "Event" = $event;
+                                            "User" = $user;
+                                            }
+    }
+
+    $winlogonTable
+}
+
+function Get-ShutdownEvent($daysOld) {
+    $winlog = Get-EventLog system -InstanceId 2147489654 -After (Get-Date).AddDays(-$daysOld)
+
+    $winlogTable = @()
+
+    for ($i=0; $i -lt $winlog.count; $i++) {
+    
+        $event = "Shutdown"
+
+        $user = "System"
+
+        $winlogTable += [pscustomobject]@{"Time" = $winlog[$i].TimeGenerated;
+                                            "Id" = $winlog[$i].EventID;
+                                         "Event" = $event;
+                                          "User" = $user;
+                                         }
+    }
+
+
+
+    $winlogTable
+}
+
+function Get-StartupEvent($daysOld) {
+    $winlog = Get-EventLog system -InstanceId 2147489653 -After (Get-Date).AddDays(-$daysOld)
+
+    $winlogTable = @()
+
+    for ($i=0; $i -lt $winlog.count; $i++) {
+    
+        $event = "Startup"
+
+        $user = "System"
+
+        $winlogTable += [pscustomobject]@{"Time" = $winlog[$i].TimeGenerated;
+                                            "Id" = $winlog[$i].EventID;
+                                         "Event" = $event;
+                                          "User" = $user;
+                                         }
+    }
+
+
+
+    $winlogTable
+       
+}
