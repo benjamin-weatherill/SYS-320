d22e0eb34fa2e6334fde1e632632de0259cac45d
Dark-Matter1 <ben.w.1516@gmail.com>
2024-03-05 14:10:12 -0500

    Added lab4 files

---
+++ PowershellLabs/Lab04-ClassActivity/Apache-Logs.ps1
@@ -0,0 +1,24 @@
+# Powershell Script
+# SYS-320 - Ben W
+
+function ApacheLogs1 {
+
+    $page = $args[0]
+    $httpCode = $args[1]
+    $browserName = $args[2]
+
+    $notfounds =  Get-Content C:\xampp\apache\logs\access.log | Select-String "$page" |  Select-String $httpCode | Select-String $browserName
+
+    $regex = [regex] "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*"
+
+    $ips = @()
+
+    for ($i=0; $i -lt $ipsUnorganized.Count; $i++){
+        $ips += [pscustomobject]@{ "IP"  = $ipsUnorganized[$i].Value; }
+    }
+
+    $ipsoften = $ips | Where-Object { $_.IP -ilike  "10.*" }
+    $counts = $ipsoften | Group-Object IP
+    $counts | Select-Object Count, Name
+
+}
---
+++ PowershellLabs/Lab04-ClassActivity/main.ps1
@@ -0,0 +1,2 @@
+. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+Apache-Logs "nomorebeans.html" "404" "Chrome"
